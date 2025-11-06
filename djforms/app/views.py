fdb8c08e021d5bc2bd3faacd60cdfa648d020e7c
Jangyashree <k.jangyashreepatra@gmail.com>
2025-11-06 10:53:00 +0530

    Validating and Accessing data

--- djforms/app/views.py
+++ djforms/app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from app.forms import *
+from django.http import HttpResponse
 # Create your views here.
 def studentdjform(request):
     ESDJF=StudentdjForm()
     d={'ESDJF':ESDJF}
+    if request.method=='POST':
+        SFDO=StudentdjForm(request.POST) #collect data
+        if SFDO.is_valid():  # for validating
+            return HttpResponse(str(SFDO.cleaned_data))
+        else:
+            return HttpResponse('Invalid data')
+              
     return render(request,'studentdjform.html',d)
