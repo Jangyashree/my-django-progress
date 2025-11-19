f5ee1a8fa1d87fb3f11145e1affa78f55a0d5346
Jangyashree <k.jangyashreepatra@gmail.com>
2025-11-19 17:02:58 +0530

    registration

--- PREG16/app/views.py
+++ PREG16/app/views.py
@@ -2,10 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.forms import *
+from django.http import HttpResponse
 
 def registration(request):
     EUMFO=UserMF()
     EPMFO=ProfileMF()
     d={'EUMFO':EUMFO,'EPMFO':EPMFO}
+
+
+    if request.method=='POST' and request.FILES:
+        NMUMFDO=UserMF(request.POST)
+        NMPMFDO=ProfileMF(request.POST,request.FILES)
+        if NMUMFDO.is_valid() and NMPMFDO.is_valid():
+            MUMFDO=NMUMFDO.save(commit=False)
+            pw=NMUMFDO.cleaned_data['password']
+            MUMFDO.set_password(pw)
+            MUMFDO.save()
+
+            #Done with user
+            #start Profile
+
+            MPMFDO=NMPMFDO.save(commit=False)
+            MPMFDO.username=MUMFDO
+            MPMFDO.save()
+            return HttpResponse('Registration is Successfull..')
+        else:
+            return HttpResponse('Invalid Data')
 
     return render(request,'registration.html',d)
