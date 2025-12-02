967e1ae8b8f8a67445d1bbe26f0a353264396e0e
Jangyashree <k.jangyashreepatra@gmail.com>
2025-12-02 19:54:20 +0530

    CBV_DetailView

--- Project19/app/models.py
+++ Project19/app/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
 class School(models.Model):
     scname=models.CharField()
     scloc=models.CharField()
     scprincipal=models.CharField()
     def __str__(self):
         return self.scname
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pk':self.pk})
+
 
 class Student(models.Model):
     stname=models.CharField()
     stage=models.IntegerField()
-    scname=models.ForeignKey(School, on_delete=models.CASCADE, related_name="School")
+    scname=models.ForeignKey(School, on_delete=models.CASCADE, related_name="students")
     def __str__(self):
         return self.stname
--- Project19/app/views.py
+++ Project19/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.models import *
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 class school_list(ListView):
     model=School
@@ -10,5 +10,9 @@ class school_list(ListView):
     #template_name='school_list.html'
     context_object_name='QSLSO'
     ordering = ['scname']
+
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='RetrieveSO'
 
     
